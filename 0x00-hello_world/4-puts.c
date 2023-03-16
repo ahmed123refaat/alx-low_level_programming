88dd0a45c03019d714349e955527a79d625ec85a
ahmed123refaat <ahmed.refaat.a12@gmail.com>
2023-03-16 08:13:18 -0700

    iurq3ui

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Aprogram that prints aline with puts function
- * * Return:0 (Success)
+ * * Return: 0 (Success)
  */
  int main(void){
 puts("\"Programming is like building a multilingual puzzle");

e9e6b14e26c5c8675c38df53a028a70c13c4c7b5
ahmed123refaat <ahmed.refaat.a12@gmail.com>
2023-03-16 08:34:04 -0700

    efey

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
-/** write thing by using printf function
+/**
+ * main - A orogram that print aline using printf function
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art \n");
+	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
 }

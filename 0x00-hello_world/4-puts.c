a002a92ce773461440b48ea504b1eeed3b3351e3
ahmed123refaat <ahmed.refaat.a12@gmail.com>
2023-03-16 08:23:07 -0700

    ejgwuieg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
  * main - Aprogram that prints aline with puts function
- * * Return: 0 (Success)
+ * Return: 0 (Success)
  */
- int main(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);

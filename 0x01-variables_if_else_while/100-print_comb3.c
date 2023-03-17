27faa82f7dd7d9f3025c081c0d60914fea2eb8aa
ahmed123refaat <ahmed.refaat.a12@gmail.com>
2023-03-17 09:06:53 -0700

    furi

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 9 ; i++)
+{
+for ( m = 1 ; m < 10 ; m++)
+{
+if (m > n)
+{
+putchar(m);
+putchar(n);
+}
+}
+}
+putchar('\n');
+return (0);
+}

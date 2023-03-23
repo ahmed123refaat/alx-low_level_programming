ae7985ce3fc627c7c9837f98901b25108290a127
ahmed123refaat <125468663+ahmed123refaat@users.noreply.github.com>
2023-03-23 14:43:47 +0200

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,19 +11,24 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
+if (i % 3 == 0 && i % 5 != 0)
 {
-printf("Fizz");
-}
-else if (i % 5 == 0)
+printf(" Fizz");
+} else if (i % 5 == 0 && i % 3 != 0)
 {
-printf("Buzz");
-}
-else if (i % 3 == 0 && i % 5 == 0)
+printf(" Buzz");
+} else if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBuzz");
+printf(" FizzBuzz");
+} else if (i == 1)
+{
+printf("%d", i);
 }
-_putchar('\n');
+else
+{
+printf(" %d", i);
 }
+}
+printf("\n");
 return (0);
 }

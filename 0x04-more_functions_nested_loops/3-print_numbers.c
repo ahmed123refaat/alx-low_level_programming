b05fa8fcdac23756ca58fc7037cc8bc2b8348be6
ahmed123refaat <ahmed.refaat.a12@gmail.com>
2023-03-23 03:56:10 -0700

    orhio

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_numbers - checks for checks for a digit (0 through 9).
+* Return: Always 0.
+*/
+void print_numbers(void)
+{
+for (i = 0 ; i < 10 ; i++)
+{
+_puthar(i);
+}
+_puthar('\n');
+}

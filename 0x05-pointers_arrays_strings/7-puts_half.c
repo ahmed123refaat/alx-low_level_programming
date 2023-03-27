afb7bf793f9a03c5612791fb7a86f380a002c67d
ahmed123refaat <ahmed.refaat.a12@gmail.com>
2023-03-27 07:47:03 -0700

    doid

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* puts_half - a function that prints half of a string
+* if odd len, n = (length_of_the_string - 1) / 2
+* @str: input
+* Return: half of input
+*/
+void puts_half(char *str)
+{
+int counter;
+int i; n;
+n = (counter - 1) / 2;
+a = (counter) / 2;
+while (*str != '\0')
+{
+counter++;
+str++;
+}
+for (i = 0 ; i <= counter ; i++)
+{
+if (i == n)
+{
+_putchar(str[i]);
+}
+if else (i == a)
+{
+_putchar(str[i]);
+}
+}
+}
+

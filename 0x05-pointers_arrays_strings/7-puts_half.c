89eb6ceaa372bcc5f67c6dc9d6fa599e4f94f430
ahmed123refaat <125468663+ahmed123refaat@users.noreply.github.com>
2023-03-27 17:12:57 +0200

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,23 +9,21 @@ void puts_half(char *str)
 {
 int counter;
 int i;
-int n;
-int a;
 while (*str != '\0')
 {
 counter++;
 str++;
 }
-n = ((counter + 1) / 2);
-a = (counter) / 2;
 for (i = 0 ; i <= counter ; i++)
 {
-if (i == n)
+if (i %2 == 1)
 {
+i = ((counter + 1) / 2);
 _putchar(str[i]);
 }
-else if (i == a)
+else if (i%2 == 0)
 {
+i = (counter) / 2;
 _putchar(str[i]);
 }
 }

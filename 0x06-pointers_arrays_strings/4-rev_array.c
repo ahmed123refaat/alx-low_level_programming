0a8f1e5893965aa173292cf9f384f5414dd68ccc
ahmed123refaat <ahmed.refaat.a12@gmail.com>
2023-03-30 11:00:00 -0700

    erknrko

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* reverse_array - reverse array of integers
+* @a: array
+* @n: number of elements of array
+* Return: void
+*/
+void reverse_array(int *a, int n)
+{
+int i;
+i = 0;
+while (i < n)
+{
+i++;
+if (i != 0)
+{
+printf(", ");
+}
+printf("%d", a[i]);
+i--;
+}
+printf("\n");
+}

db7dc230fe828a56eb6b1c28bdb5972a6361bafa
ahmed123refaat <125468663+ahmed123refaat@users.noreply.github.com>
2023-04-03 17:57:22 +0200

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,13 +7,15 @@
 */
 char *_strpbrk(char *s, char *accept)
 {
-int i;
+int k;
 while (*s)
 {
-for (i = 0; accept[i]; i++)
+for (k = 0; accept[k]; k++)
 {
 if (*s == accept[k])
 return (s);
+}
+s++;
 }
 return ('\0');
 }

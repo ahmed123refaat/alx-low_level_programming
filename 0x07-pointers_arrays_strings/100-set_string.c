bffecbc09eeb380fed25173975aa84a2de389061
ahmed123refaat <125468663+ahmed123refaat@users.noreply.github.com>
2023-04-03 20:32:10 +0200

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,11 @@
 #include"main.h"
 /*
-* print_sets the value of a pointer to a char. - Entry point
-* @s: pointer to pointer
-* @to: pointer
+* set_string - Entry point
+* @s: input
+* @to: input
 * Return: Always 0 (Success)
 */
 void set_string(char **s, char *to)
 {
-s = &to;
+*s = &to;
 }

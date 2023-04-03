ee7588f5684b863bea5dbb1e7f5c42e03c6ef5e0
ahmed123refaat <ahmed.refaat.a12@gmail.com>
2023-04-03 11:21:22 -0700

    kt

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/*
+* print_sets the value of a pointer to a char. - Entry point
+* @s: input
+* @to: input
+* Return: Always 0 (Success)
+*/
+void set_string(char **s, char *to)
+{
+char c;
+to = &c;
+s = &to;
+}

d97f683356981766dfa1ffe952f9ce7827b6e096
ahmed123refaat <ahmed.refaat.a12@gmail.com>
2023-04-05 10:10:15 -0700

    kejek

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _puts_recursion - function like puts();
+* @s: input
+* Return: Always 0 (Success)
+*/
+void _puts_recursion(char *s)
+{
+if (s != '\0')
+{
+_putchar(s);
+}
+}

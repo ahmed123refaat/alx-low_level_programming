c4534bc11e7b0f55e36b7ae4fdfba5ced6ecdfc4
ahmed123refaat <ahmed.refaat.a12@gmail.com>
2023-04-06 06:13:15 -0700

    my updates

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+int i;
+while (*s != '\0')
+{
+i++;
+s++
+}
+return (i);
+}

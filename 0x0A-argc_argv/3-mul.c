c9c325b38c60f15bd7b5aa46c14c308788614ad1
ahmed123refaat <ahmed.refaat.a12@gmail.com>
2023-04-07 08:47:24 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/*
+* _atoi - converts a string to an integer
+* @s: string to be converted
+* Return: the int converted from the string
+*/
+int _atoi(char *s)
+{
+return atoi(s);
+}
+

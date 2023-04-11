a7448b6f3bfce2eb3be2e130c8671e23e9f9ddae
ahmed123refaat <125468663+ahmed123refaat@users.noreply.github.com>
2023-04-11 20:14:55 +0200

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,9 +10,7 @@ char *str_concat(char *s1, char *s2)
 {
 int i;
 int r;
-char *c;
-char *z;
-char *e;
+char *const
 while (s1[i] != '\0' || s2[r] != '\0')
 i++;
 r++;
@@ -20,8 +18,19 @@ if (s1 == NULL && s2 == NULL)
 {
 return (NULL);
 }
-*c = malloc(sizeof(s1) + 1);
-*z = malloc(sizeof(s2) + 1);
-*e = *c + *z;
-return (e);
+conct = malloc(sizeof(char) * (i + ci + 1));
+if (conct == NULL)
+return (NULL);
+while (s1[i] != '\0')
+{
+conct[i] = s1[i];
+i++;
+}
+while (s2[ci] != '\0')
+{
+conct[i] = s2[ci];
+i++, ci++;
+}
+conct[i] = '\0';
+return (*const);
 }

d38b68c8583494ecc7f06fb8979f9504b6f4ba04
ahmed123refaat <125468663+ahmed123refaat@users.noreply.github.com>
2023-04-17 21:13:25 +0200

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,5 @@
 #ifndef DOG_H
+
 #define DOG_H
 /**
 * struct dog - a dog's basic info
@@ -9,14 +10,14 @@
 */
 struct dog
 {
-char *name; 
+char *name;
 float age;
 char *owner;
-}
+};
 /**
 * dog_t - typedef for struct dog
 */
- typedef struct dog dog_t;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);

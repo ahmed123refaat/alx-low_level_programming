a3df27d84b9f5c2bff8b817133f857cf65ed5267
ahmed123refaat <ahmed.refaat.a12@gmail.com>
2023-04-28 09:05:23 -0700

    updates

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+* add_nodeint_end - adds a node at the end of a linked list
+* @head: pointer to the first element in the list
+* @n: data to insert in the new element
+*
+* Return: pointer to the new node, or NULL if it fails
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+listint_t *p;
+listint_t *t;
+if (!p)
+return (NULL);
+p = malloc(sizeof(listint_t));
+p->n = n;
+p->next = NULL;
+if (*head == NULL)
+head = p;
+return (p);
+while (t->next)
+t = t->next;
+t->next = p;
+return (p);
+}

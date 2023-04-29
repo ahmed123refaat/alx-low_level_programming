c5ffd4d8febc27fa66732f77115c666ff271c165
ahmed123refaat <125468663+ahmed123refaat@users.noreply.github.com>
2023-04-29 13:35:50 +0200

    Update 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,5 +7,13 @@
 */
 int pop_listint(listint_t **head)
 {
-head = head->next;
+listint_t *temp;
+int num;
+if (!head || !*head)
+return (0);
+num = (*head)->n;
+temp = (*head)->next;
+free(*head);
+*head = temp;
+return (num);
 }

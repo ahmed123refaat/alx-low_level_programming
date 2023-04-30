8fc26feb4a78d3f8de04fac3718921498e3de627
ahmed123refaat <125468663+ahmed123refaat@users.noreply.github.com>
2023-04-30 16:35:08 +0200

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,9 +11,10 @@ unsigned int i = 0;
 listint_t *temp = head;
 while (temp != NULL && i < index)
 {
+temp = temp->next;
 i++;
 if (i == index)
-temp = temp->next;
+temp->next = head;
 }
 temp->next = NULL;
 return (temp);
